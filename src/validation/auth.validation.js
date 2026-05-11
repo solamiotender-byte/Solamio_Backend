6b54a1050c46fe4e4e0ae9845a11bf4895d24f07
premananda <premananda.vanurtech@gmail.com>
2026-05-11 10:49:15 +0530

    copy 360

--- src/validation/auth.validation.js
+++ src/validation/auth.validation.js
@@ -40,20 +40,11 @@ export const validateLogin = [
     .withMessage("email is required")
     .isEmail()
     .withMessage("Invalid email address")
-    .trim(),
+    .trim()
+    .toLowerCase(),
 
   body("password")
     .notEmpty()
     .withMessage("password is required")
-    .isLength({ min: 8 })
-    .withMessage("Password must be at least 8 characters long")
-    .matches(/\d/)
-    .withMessage("Password must contain at least one number")
-    .matches(/[A-Z]/)
-    .withMessage("Password must contain at least one uppercase letter")
-    .matches(/[a-z]/)
-    .withMessage("Password must contain at least one lowercase letter")
-    .matches(/[@$!%*?&]/)
-    .withMessage("Password must contain at least one special character")
     .trim(),
 ];
